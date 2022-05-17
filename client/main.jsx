2d1042f83ab5b423a2ddbedff87350c7fde66993
Leghis <ayinamaerik@gmail.com>
2022-05-17 16:25:03 +0100

    élaboration de meteor, ssr et integration de apollo

--- client/main.jsx
+++ client/main.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import { onPageLoad } from "meteor/server-render";
-import { App } from '/imports/ui/App';
+import {onPageLoad} from "meteor/server-render";
+import {App} from '/imports/ui/App';
 import {renderToString} from "react-dom/server";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider, HttpLink
+} from "@apollo/client";
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/',
+//   cache: new InMemoryCache()
+// });
+
+//connecting the Graphql client to the Apollo server
+const cache = new InMemoryCache();
+let client = new ApolloClient({
+  link: new HttpLink({uri: 'http://localhost:4000', fetch}),
+  cache
+})
 
 onPageLoad(sink => {
   sink.renderIntoElementById("react-target", renderToString(
-    <App />
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
   ))
 })
-
-
-
-
-
-// import React from 'react';
-// import { Meteor } from 'meteor/meteor';
-// import { render } from 'react-dom';
-// import { App } from '/imports/ui/App';
-//
-// Meteor.startup(() => {
-//   render(<App/>, document.getElementById('react-target'));
-// });
--- server/main.js
+++ server/main.js
@@ -1,51 +1,26 @@
 import React from "react";
-import { renderToString } from "react-dom/server";
-import { onPageLoad } from "meteor/server-render";
+import {renderToString} from "react-dom/server";
+import {onPageLoad} from "meteor/server-render";
+import fetch from 'cross-fetch';
+import {App} from "../imports/ui/App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider, HttpLink
+} from "@apollo/client";
 
-import { App } from "../imports/ui/App";
 
 onPageLoad(sink => {
+  const cache = new InMemoryCache();
+  const context = {
+    client: new ApolloClient({
+      link: new HttpLink({uri: 'http://localhost:4000', fetch}),
+      cache
+    })
+  };
   sink.renderIntoElementById("app", renderToString(
-    <App location={sink.request.url} />
+    <ApolloProvider client={context.client}>
+      <App location={sink.request.url}/>
+    </ApolloProvider>
   ));
 });
-
-
-
-
-
-
-
-
-
-// import { Meteor } from 'meteor/meteor';
-// import { LinksCollection } from '/imports/api/links';
-//
-// function insertLink({ title, url }) {
-//   LinksCollection.insert({title, url, createdAt: new Date()});
-// }
-
-// Meteor.startup(() => {
-//   // If the Links collection is empty, add some data.
-//   if (LinksCollection.find().count() === 0) {
-//     insertLink({
-//       title: 'Do the Tutorial',
-//       url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-//     });
-//
-//     insertLink({
-//       title: 'Follow the Guide',
-//       url: 'http://guide.meteor.com'
-//     });
-//
-//     insertLink({
-//       title: 'Read the Docs',
-//       url: 'https://docs.meteor.com'
-//     });
-//
-//     insertLink({
-//       title: 'Discussions',
-//       url: 'https://forums.meteor.com'
-//     });
-//   }
-// });
